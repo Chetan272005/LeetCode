bc6af331a8f1c5a0178613f1d0f623e10f741d23
Chetan272005 <805chetan@gmail.com>
2026-08-04 16:00:40 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (11.79%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,12 +1,10 @@
 int missingNumber(int* nums, int numsSize) {
    int sum=0;
-   int sum2=0; 
+   int sum2=(numsSize*(numsSize+1))/2;
 
    for(int i=0; i < numsSize ; i++){
     sum = sum + nums[i];
    }
-
-   sum2=(numsSize*(numsSize+1))/2;
 
    return sum2-sum;
 }
