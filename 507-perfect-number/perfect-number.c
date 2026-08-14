9243e367be7d1b2db1743ede901460407f242f3d
Chetan272005 <805chetan@gmail.com>
2026-08-14 19:07:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (88.37%) - LeetSync

--- 507-perfect-number/perfect-number.c
+++ 507-perfect-number/perfect-number.c
@@ -1,16 +1,17 @@
 #include <stdbool.h>
 
 bool checkPerfectNumber(int num){
-    int sum=0;
-    for(int i = 1; i <= num / 2; i++){
-    if(num % i == 0){
-        sum += i;
-    }
-}
-    if(sum == num){
-        return true;
-    }
-    else{
+    if(num <= 1){
         return false;
     }
+    int sum=1;
+    for(int i = 2; i*i <= num; i++){
+    if(num % i == 0){
+        sum += i;
+        if(i != num/i){
+        sum += num/i;
+    }
+    }
+}
+    return sum == num;
 }
