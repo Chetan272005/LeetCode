0748d41a14985f2165ab0b352b73b7c6afd5045d
Chetan272005 <805chetan@gmail.com>
2026-08-15 12:44:36 +0530

    Time: 13 ms (51.72%) | Memory: 12.3 MB (66.70%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.c
+++ 1580-shuffle-the-array/shuffle-the-array.c
@@ -6,9 +6,9 @@ int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
 
     int k = 0;
 
-    for (int i = 0; i < numsSize; i += 2) {
-        ans[i] = nums[k];
-        ans[i + 1] = nums[k + n];
+    for (int i = 0; i < n; i++) {
+        ans[2*i] = nums[k];
+        ans[2*i + 1] = nums[k + n];
         k++;
     }
 
