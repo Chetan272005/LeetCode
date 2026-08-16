bf64a0a32dd1e25afd0b07009aff5f7804fe84f9
Chetan272005 <805chetan@gmail.com>
2026-08-16 15:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 25.8 MB (30.69%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.c
@@ -0,0 +1,12 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* decode(int* encoded, int encodedSize, int first, int* returnSize) {
+    *returnSize = encodedSize + 1;
+    int* decoded =(int*)malloc((*returnSize)*sizeof(int));
+    decoded[0]=first;
+    for(int i=0; i<encodedSize; i++){
+        decoded[i+1]=decoded[i]^encoded[i];
+    }
+    return decoded;
+}
