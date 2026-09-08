5a21349e0db0b18fe714f644f979aea0ca6c1ad8
Chetan272005 <805chetan@gmail.com>
2026-09-08 06:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (11.03%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.c
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.c
@@ -1,11 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
 int count(struct TreeNode* root) {
     if (root == NULL) {
         return 0;
@@ -14,22 +6,37 @@ int count(struct TreeNode* root) {
     return 1 + count(root->left) + count(root->right);
 }
 
-void inorder(struct TreeNode* root, int arr[], int* index) {
+int inorder(struct TreeNode* root, int arr[], int* index, int k) {
     if (root == NULL) {
-        return;
+        return 0;
     }
 
-    inorder(root->left, arr, index);
+    inorder(root->left, arr, index, k);
+
+    if (*index == k) {
+        return arr[k - 1];
+    }
 
     arr[*index] = root->val;
     (*index)++;
 
-    inorder(root->right, arr, index);
+    if (*index == k) {
+        return arr[k - 1];
+    }
+
+    return inorder(root->right, arr, index, k);
 }
+
 int kthSmallest(struct TreeNode* root, int k) {
     int n = count(root);
+
     int* arr = (int*)malloc(n * sizeof(int));
+
     int index = 0;
-    inorder(root, arr, &index);
-    return arr[k-1];
+
+    int answer = inorder(root, arr, &index, k);
+
+    free(arr);
+
+    return answer;
 }
